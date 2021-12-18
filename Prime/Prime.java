9e03db22d07f2c8340bcdec699816976b159d2b5
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-18 12:00:56 +0530

    Create Prime.java

---
+++ Prime/Prime.java
@@ -0,0 +1,39 @@
+ 
+
+ public class Prime { 
+
+         public static void main(String[] args) { 
+
+                 int i,temp=0,j; 
+
+                 for(i=1;i<=100;i++) { 
+
+                         for(j=1;j<=100;j++) { 
+
+                                 if(i%j==0) { 
+
+                                         temp++; 
+
+                                 } 
+
+                         } 
+
+                         if(temp ==2) { 
+
+                                 System.out.println(""+i); 
+
+                         } 
+
+                         else { 
+
+                                 temp =0; 
+
+                         } 
+
+                 } 
+
+         } 
+
+  
+
+ }
