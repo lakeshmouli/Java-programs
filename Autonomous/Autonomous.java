94a26372e5badc3915c8339dc81eb8ea19bc5364
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-18 12:36:16 +0530

    Create Autonomous.java

---
+++ Autonomous/Autonomous.java
@@ -0,0 +1,43 @@
+ 
+
+ abstract class Abs_Class 
+
+ { 
+
+         public abstract void mymethod(); 
+
+ } 
+
+ public class Anonymous_class { 
+
+  
+
+         public static void main(String[] args) { 
+
+                  
+
+        Abs_Class obj = new Abs_Class() 
+
+                         { 
+
+                         public void mymethod() 
+
+                         { 
+
+                                 System.out.println("Anonymous Inner class"); 
+
+                         } 
+
+                         }; 
+
+                         obj.mymethod(); 
+
+          
+
+                  
+
+         } 
+
+  
+
+ }
